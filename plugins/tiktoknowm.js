8c45a00dbeec90032d0d7b6e5300df352534f61f
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-02-17 20:25:40 +0700

    Update tiktoknowm.js

--- plugins/tiktoknowm.js
+++ plugins/tiktoknowm.js
@@ -1,8 +1,8 @@
 const {tiktokdownload} = require('tiktok-scraper-without-watermark')
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, text }) => {
   m.reply(wait)
-  api = await tiktokdownload(m.text)
+  api = await tiktokdownload(text)
   conn.sendFile(m.chat, api.nowm, '', wm, m)
 }
 
