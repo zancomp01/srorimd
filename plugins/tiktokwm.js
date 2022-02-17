748bf8cfed8ceb00a4842fda9a378d120db3de61
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-02-17 20:18:49 +0700

    Update tiktokwm.js

--- plugins/tiktokwm.js
+++ plugins/tiktokwm.js
@@ -1,10 +1,10 @@
 const {tiktokdownload} = require('tiktok-scraper-without-watermark')
 
 let handler = async (m, { conn }) => {
-  m.reply(wait)
-  console.log(m.text)
-  api = await tiktokdownload(m.text)
-  conn.sendFile(m.chat, api.wm, '', wm, m)
+  await m.reply(wait)
+  m.reply(m.text)
+  //api = await tiktokdownload(m.text)
+  //conn.sendFile(m.chat, api.wm, '', wm, m)
 }
 
 handler.command = /^(tiktokwm)$/i
