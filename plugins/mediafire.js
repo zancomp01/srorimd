6bcb31221d7cc4971a70a8818e7d0790b14118af
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-02-17 19:10:17 +0700

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -2,12 +2,12 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `uhm.. urlnya mana?\n\npenggunaan:\n${usedPrefix + command} url\ncontoh:\n${usedPrefix + command} http://www.mediafire.com/file/js0gr2nozcmk9yg/example.txt/file`
-    let res = await fetch(API('melcanz', '/mediafire', { url: text }, 'apikey'))
+    let res = await fetch(API('lol', '/api/mediafire', { url: text }, 'apikey'))
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
     await m.reply(wait)
-    await conn.sendFile(m.chat, json.result.link, json.result.nama, wm, m)
+    await conn.sendFile(m.chat, json.result.link, json.result.filename, wm, m)
 }
 handler.help = ['mediafire'].map(v => v + ' <url>')
 handler.tags = ['downloader']
