730338fa3008e786233ec7b211a465fe114d14f6
fafhghg1 <94165730+fafhghg1@users.noreply.github.com>
2022-02-17 19:55:52 +0700

    Create tiktok.js

---
+++ plugins/tiktok.js
@@ -0,0 +1,13 @@
+const {tiktokdownload} = require('tiktok-scraper-without-watermark')
+
+let handler = async (m, { conn }) => {
+  m.reply(wait)
+  api = await tiktokdownload(m.text)
+  conn.sendFile(m.chat, api.nowm, '.mp4', wm, m)
+}
+
+handler.command = /^(tiktok)$/i
+handler.help = ['tiktok']
+handler.tags = ['downloader']
+
+module.exports = handler
